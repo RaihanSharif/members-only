37703503aa37e3283cb5fdb32411e1580900eac2
Raihan Sharif <raihansharif92@gmail.com>
2025-11-12 14:55:56 +0000

    sign up without validation

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -7,14 +7,29 @@
     b. call some qery to insert user into db, first checking that username, email or email aren't already
        in the system
 */
+const bcrypt = require("bcryptjs");
 
-function getSignupForm(req, res) {
+const pool = require("../db/pool");
+
+function getSignupForm(req, res, next) {
   console.log("rendering sign up form");
   res.render("sign-up", { title: "sign up to be a member" });
+  next();
 }
 
-function postSignupForm(req, res) {
-  res.json(req.body);
+async function postSignupForm(req, res, next) {
+  try {
+    console = hashedPassword = await bcrypt.hash(req.body.password, 12);
+
+    const data = req.body;
+    await pool.query(
+      "INSERT INTO account (fname, lname, username, email, password) VALUES ($1, $2, $3, $4, $5)",
+      [data.fname, data.lname, data.username, data.email, hashedPassword]
+    );
+    res.redirect("/");
+  } catch (err) {
+    return next(err);
+  }
 }
 
 module.exports = {
