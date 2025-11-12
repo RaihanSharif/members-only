212a49cd40ea917f015e32189ca71856f8c416d1
Raihan Sharif <raihansharif92@gmail.com>
2025-11-12 14:21:22 +0000

    sign up form password and confirm password fields must match

--- app.js
+++ app.js
@@ -2,12 +2,24 @@ const express = require("express");
 const app = express();
 const path = require("node:path");
 const mountRoutes = require("./routes/indexRoute.js");
+const passport = require("./middlewares/authMiddleware.js");
+const session = require("express-session");
 
 const assetsPath = path.join(__dirname, "public");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(assetsPath));
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.session());
 
 console.log("app.js, mounting routes...");
 mountRoutes(app);
