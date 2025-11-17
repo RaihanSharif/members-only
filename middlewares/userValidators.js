c7d3252a987e4b9ddb8c4e5a1645f1c70791d7a9
Raihan Sharif <raihansharif92@gmail.com>
2025-11-17 07:45:22 +0000

    remove escape() on user account input

--- middlewares/userValidators.js
+++ middlewares/userValidators.js
@@ -1,16 +1,10 @@
 const { body } = require("express-validator");
 const pool = require("../db/pool");
 
-//TODO: create a queries file
-
-// user creation, login, update validators
-
 const alphaErr = "must only contain letters.";
 const lengthErr = "must be between 1 and 10 characters";
-
 const alphaNumericErr = "must only contain letters and numbers";
 
-// TODO: isStrongPassword validator research
 const validateUser = [
   body("fname")
     .trim()
@@ -47,23 +41,18 @@ const validateUser = [
       }
     })
     .normalizeEmail(),
-  // { minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1,
-  // returnScore: false, pointsPerUnique: 1, pointsPerRepeat: 0.5, pointsForContainingLower: 10,
-  // pointsForContainingUpper: 10, pointsForContainingNumber: 10, pointsForContainingSymbol: 10 }
   body("password")
     .trim()
     .isStrongPassword()
     .withMessage(
       "password must be at least 8 characters long, contain 1 lowercase letter, one uppercase letter, and 1 symbol"
-    )
-    .escape(),
+    ),
   body("confirm-password")
     .trim()
     .isStrongPassword()
     .withMessage(
       "password must be at least 8 characters long, contain 1 lowercase letter, one uppercase letter, and 1 symbol"
     )
-    .escape()
     .custom((value, { req, loc, path }) => {
       if (value !== req.body.password) {
         throw new Error("passwords do not match");
